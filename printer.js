628656595975943a7629611a358a245d8c88d3a5
nino-porcino <nino.porcino@gmail.com>
2018-09-14 20:31:48 +0200

    lprinter writes as a single string

--- printer.js
+++ printer.js
@@ -3,6 +3,28 @@
 let printerBuffer = "";
 let printerReady = 0x00;
 
+let printerTimeLastReceived ;
+
+// this version prints the whole buffer into one console line, allowing copy & paste
+// print is done if nothing is received from Laser within 2 seconds
+function checkPrinterBuffer() {
+   const d = new Date() - printerTimeLastReceived;
+   if(d > 2000 && printerBuffer !== "") {
+      console.log(printerBuffer);
+      printerBuffer = "";
+      return;
+   }
+   setTimeout(()=>checkPrinterBuffer(), 2000);
+}
+
+function printerWrite(byte) {
+   printerBuffer += String.fromCharCode(byte & 0xFF);
+   printerTimeLastReceived = new Date();
+   checkPrinterBuffer();
+}
+
+/*
+// this version prints one line on one console line
 function printerWrite(byte) {
    printerBuffer += String.fromCharCode(byte & 0xFF);
    if(byte == 0x0A) {
@@ -10,3 +32,4 @@ function printerWrite(byte) {
       printerBuffer = "";
    }
 }
+*/
