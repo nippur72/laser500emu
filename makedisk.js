b8c7200bab97649c6c9aa358ce9949d716c91883
nino-porcino <nino.porcino@gmail.com>
2018-10-22 16:29:24 +0200

    add tool for making .nic disk files

---
+++ makedisk.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const charset = fs.readFileSync("4_s0.nic");
+
+let s = "const disk_image = new Uint8Array([\n   ";
+
+// normal set
+charset.forEach((value, i)=> {      
+   const cr = (i % 16 == 15) ? '\n   ' : '';
+   s += `${hex(value)},${cr}`;   
+});
+
+s+="]);";
+
+console.log(s);
+
+function hex(value) {
+   return "0x" + (value<=0xF ? "0":"") + value.toString(16);
+}
