5db8360eb4a2e831e86291bacc0c733d7b4edd78
nino-porcino <nino.porcino@gmail.com>
2020-04-16 10:02:58 +0200

    correct framerate frequency

--- emulator.js
+++ emulator.js
@@ -115,7 +115,7 @@ let cpu = new Z80({ mem_read, mem_write, io_read, io_write });
 const frameRate = 49.7; // 50 Hz PAL standard
 const frameDuration = 1000/frameRate; // duration of 1 frame in msec
 const cpuSpeed = /*3694682.5;*/ 3672000;  /*for 312 */ //3694700; // Z80 speed 3.6947 MHz (NEC D780c)
-const cyclesPerLine = (cpuSpeed / frameRate / TOTAL_SCANLINES); // 188.5=basic OK; 196=sound OK;
+let cyclesPerLine = 237.45; //(cpuSpeed / frameRate / TOTAL_SCANLINES); // 188.5=basic OK; 196=sound OK;
 const HIDDEN_LINES = 2;
 
 // 3694682.5 - 192 * 80 - 312 * 20
